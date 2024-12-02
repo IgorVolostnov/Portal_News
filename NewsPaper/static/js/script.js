f1c0992b5614a22a315b628b32a2892ca76854d7
Igor Volostnov <visfin@yandex.ru>
2024-12-03 00:27:56 +0300

    Commit 202412030027

--- NewsPaper/static/js/script.js
+++ NewsPaper/static/js/script.js
@@ -1,13 +1,18 @@
 function openNav() {
 const myNav = document.getElementById("myNav")
-myNav.style.height = "70%";
+const screenHeight = window.screen.height;
+let size = myNav.scrollHeight;
+let sizePercent = String((size + (size * 0,70)) * 100 / screenHeight) + "%";
+myNav.style.height = sizePercent;
 this.style = "display: none;";
 myNav.onmouseleave = function() {
  myNav.style.height = "0%";
  };
  }
 
+
 function closeNav() {
 const myNav = document.getElementById("myNav")
 myNav.style.height = "0%";
  }
+
