24853254803f85c33742d6f8767b90a2bd830b29
Igor Volostnov <visfin@yandex.ru>
2024-12-11 01:44:00 +0300

    Commit 202412110143

---
+++ NewsPaper/news/filters.py
@@ -0,0 +1,28 @@
+import datetime
+import locale
+from django.forms import CheckboxSelectMultiple, TextInput, SelectDateWidget, DateInput
+from django_filters import FilterSet, CharFilter, DateFilter, MultipleChoiceFilter
+from .models import Post
+
+locale.setlocale(category=locale.LC_ALL, locale="Russian")
+
+class MyDateInput(DateInput):
+    input_type = 'date'
+    format = '%Y-%m-%d'
+
+
+class PostFilter(FilterSet):
+    TypePost = MultipleChoiceFilter(label='Вид контента:', field_name='type_post', choices=Post.KIND_CONTENT,
+                                    widget=CheckboxSelectMultiple(attrs={'class': 'category-checkbox'}))
+    Title = CharFilter(label='Заголовок содержит:', field_name='title_post', lookup_expr='icontains',
+                       widget=TextInput(attrs={'class': 'title-input'}))
+    TextPost = CharFilter(label='Текст содержит:', field_name='text_post', lookup_expr='icontains',
+                          widget=TextInput(attrs={'class': 'text-input'}))
+    DateLt = DateFilter(label='Дата меньше:', field_name='time_in_post', lookup_expr='lt',
+                        widget=MyDateInput(attrs={'class': 'date-lt-input'}))
+    DateGt = DateFilter(label='Дата больше:', field_name='time_in_post', lookup_expr='gt',
+                        widget=TextInput(attrs={'class': 'date-gt-input'}))
+    class Meta:
+        model = Post
+        fields = '__all__'
+        exclude = ['author_post', 'type_post', 'time_in_post', 'category_post', 'title_post', 'text_post', 'rating_post']
--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -1,6 +1,7 @@
 import datetime
 from django.views.generic import ListView, DetailView
 from .models import Post
+from .filters import PostFilter
 
 
 class PostList(ListView):
@@ -17,6 +18,20 @@ class PostList(ListView):
     # Метод get_context_data позволяет нам изменить набор данных,
     # который будет передан в шаблон.
     paginate_by = 2  # вот так мы можем указать количество записей на странице
+
+    # Переопределяем функцию получения списка товаров
+    def get_queryset(self):
+        # Получаем обычный запрос
+        queryset = super().get_queryset()
+        # Используем наш класс фильтрации.
+        # self.request.GET содержит объект QueryDict, который мы рассматривали
+        # в этом юните ранее.
+        # Сохраняем нашу фильтрацию в объекте класса,
+        # чтобы потом добавить в контекст и использовать в шаблоне.
+        self.filterset = PostFilter(self.request.GET, queryset)
+        # Возвращаем из функции отфильтрованный список товаров
+        return self.filterset.qs
+
     def get_context_data(self, **kwargs):
         # С помощью super() мы обращаемся к родительским классам
         # и вызываем у них метод get_context_data с теми же аргументами,
@@ -28,6 +43,8 @@ class PostList(ListView):
         # Добавим ещё одну пустую переменную,
         # чтобы на её примере рассмотреть работу ещё одного фильтра.
         context['next_news'] = f"Свежие новости на {datetime.datetime.strftime(datetime.datetime.now(), '%d.%m.%Y')}: "
+        # Добавляем в контекст объект фильтрации.
+        context['filterset'] = self.filterset
         return context
 
 
